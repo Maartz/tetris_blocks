cf8f09b4a4133722fd64ba7188d9f3d6289650c8
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-22 19:50:13 +0100

    Create mirror/2 function to mirror if the brick.reflection is set to true

--- lib/points.ex
+++ lib/points.ex
@@ -1,19 +1,24 @@
 defmodule Tetris.Points do
   def translate(points, {x, y}) do
-    Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end )
+    Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end)
   end
 
   def transpose(points) do
     points
-    |> Enum.map( fn {x, y} -> {y, x} end )
+    |> Enum.map(fn {x, y} -> {y, x} end)
   end
+
   def mirror(points) do
     points
-    |> Enum.map( fn {x, y} -> {5-x, y} end )
+    |> Enum.map(fn {x, y} -> {5 - x, y} end)
   end
+
+  def mirror(points, false), do: points
+  def mirror(points, true), do: mirror(points)
+
   def flip(points) do
     points
-    |> Enum.map( fn {x, y} -> {x, 5-y} end )
+    |> Enum.map(fn {x, y} -> {x, 5 - y} end)
   end
 
   def rotate_90(points) do
@@ -23,6 +28,7 @@ defmodule Tetris.Points do
   end
 
   def rotate(points, 0), do: points
+
   def rotate(points, degrees) do
     rotate(
       rotate_90(points),
@@ -34,9 +40,9 @@ defmodule Tetris.Points do
     map =
       points
       |> Enum.map(fn key -> {key, "■"} end)
-      |> Map.new
+      |> Map.new()
 
-    for y <- (1..4), x <- (1..4) do
+    for y <- 1..4, x <- 1..4 do
       Map.get(map, {x, y}, "□")
     end
     |> Enum.chunk_every(4)
@@ -45,8 +51,7 @@ defmodule Tetris.Points do
   end
 
   def print(points) do
-    IO.puts __MODULE__.to_string(points)
+    IO.puts(__MODULE__.to_string(points))
     points
   end
 end
-
